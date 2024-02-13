f3eb24daf0de5a93ba368bec2ec843e1f0edcda6
André J. Kelling <kontakt@andrekelling.de>
2024-02-13 14:47:04 +0100

    CI try esbuild for demo and dist builds

--- docs/app-demo.js
+++ docs/app-demo.js
@@ -1,6 +1,7 @@
-import {logoLandscapeString} from "logoLandscapeString";
-import {logoPortraitString} from "logoPortraitString";
-import {qrCodeString} from "qrCodeString";
+import {logoLandscapeString} from "./base64-pictures/logo-landscape-string";
+import {logoPortraitString} from "./base64-pictures/logo-portrait-string";
+import {qrCodeString} from "./base64-pictures/qr-code-string";
+import printPDF from "../src/app";
 
 const basePrintData = {
     "logo": logoLandscapeString,
--- src/app.js
+++ src/app.js
@@ -1,7 +1,2 @@
 import printPDF from './print';
-
-if (window) {
-    window.printPDF = printPDF;
-}
-
 export default printPDF;
