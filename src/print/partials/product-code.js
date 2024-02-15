b398c170c88a92ac2df4b04109965ab081c09e85
André J. Kelling <kontakt@andrekelling.de>
2024-02-15 11:42:42 +0100

    PRINT add shrink approach for productCode fontSize if string is to wide

--- src/print/partials/product-code.js
+++ src/print/partials/product-code.js
@@ -1,4 +1,5 @@
 import JsPDF from 'jspdf';
+import { mmDpiFactor } from "../utils/mm-dpi-factor.js";
 
 /**
  * JsPDF import just for typing!
@@ -15,5 +16,12 @@ export default (doc, productCode, pageWidth) => {
     let startY = 55;
 
     doc.setFontSize(doc.vars.fontSizes.SmallFontSize);
+
+    // check if the product code is too long for the page ONCE
+    const stringWidthInMm = doc.getStringUnitWidth(productCode) * mmDpiFactor;
+    if (stringWidthInMm > pageWidth) {
+        doc.setFontSize(doc.vars.fontSizes.SmallFontSize - 2);
+    }
+
     doc.text(productCode, pageCenterX, startY, {align: 'center'});
 }
--- src/print/utils/mm-dpi-factor.js
+++ src/print/utils/mm-dpi-factor.js
@@ -1,1 +1,6 @@
+/**
+ * Points or pixels divide by 72 to get inches or divide by (72/25.6) to get 'mm'.
+ *
+ * @type {number}
+ */
 export const mmDpiFactor = 72 / 25.4; // dpi / mm
