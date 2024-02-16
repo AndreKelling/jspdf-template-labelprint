6f949cb92beac8d8fb028f1a30dd2151863427a3
André J. Kelling <kontakt@andrekelling.de>
2024-02-16 09:54:49 +0100

    PACKAGE release jspdf-product-label-54x70

--- src/print/partials/product-id.js
+++ src/print/partials/product-id.js
@@ -19,8 +19,5 @@ export default (doc, productId, pageWidth) => {
 
     const textOptions = {charSpace: 0.5};
 
-    // https://github.com/parallax/jsPDF/issues/3406#issuecomment-1092298117
-    const adjustmentX = textOptions.charSpace * doc.getStringUnitWidth(productId);
-
-    doc.text(productId, startX - adjustmentX, startY, textOptions);
+    doc.text(productId, startX, startY, textOptions);
 }
